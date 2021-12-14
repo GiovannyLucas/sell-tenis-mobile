f2a967aaca6c362b1deda2e7980f83489467d8e3
GiovannyLucas <giovannylucax@gmail.com>
2021-12-13 22:52:41 -0300

    feat: :sparkles: added database connection and configuration to migrations and environments

---
+++ backend/src/shared/infra/typeorm/index.ts
@@ -0,0 +1,41 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { resolve } from 'path';
+
+export const database: TypeOrmModuleOptions = {
+  type: 'postgres',
+  port: Number(process.env.DB_PORT) || 5432,
+  host: process.env.DB_HOST || 'localhost',
+  username: process.env.DB_USER || 'giovanny',
+  password: process.env.DB_PASS || 'dv1010aa',
+  database: process.env.DB_NAME || 'certsys_test',
+  entities: [
+    resolve(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'modules',
+      '**',
+      'infra',
+      'typeorm',
+      'entities',
+      '*',
+    ),
+  ],
+  migrations: [
+    resolve(__dirname, '..', 'shared', 'infra', 'typeorm', 'migrations', '*'),
+  ],
+  cli: {
+    migrationsDir: resolve(
+      __dirname,
+      '..',
+      'shared',
+      'infra',
+      'typeorm',
+      'migrations',
+    ),
+  },
+  migrationsRun: true,
+  synchronize: false,
+  logging: ['error', 'warn'],
+};
