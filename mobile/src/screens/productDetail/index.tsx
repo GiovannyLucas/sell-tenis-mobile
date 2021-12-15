4f34e91c50e4015d7995d9d722db82512b342335
GiovannyLucas <giovannylucax@gmail.com>
2021-12-15 15:26:26 -0300

    feat: :sparkles: added function to get the data, navigation back and icons

--- mobile/src/screens/productDetail/index.tsx
+++ mobile/src/screens/productDetail/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import { useNavigation } from '@react-navigation/core';
+import React, { useEffect, useState } from 'react';
 import { Text, View, Image, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { getProductById, Product } from '../../services/api/products';
 import { styles } from './styles';
 
 interface Props {
@@ -7,35 +10,80 @@ interface Props {
 }
 
 export const ProductDetailsPage = ({ route }: Props) => {
-  const {} = route.params;
+  const { id } = route.params;
+
+  const navigation = useNavigation();
+
+  const [product, setProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    async function call() {
+      try {
+        const data = await getProductById(id);
+
+        setProduct(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    call();
+  }, [id]);
 
   return (
-    <View style={{ justifyContent: 'space-around', flex: 1 }}>
+    <View style={styles.container}>
+      <View style={styles.iconsBox}>
+        <TouchableOpacity
+          style={styles.icons}
+          onPress={() => navigation.goBack()}>
+          <Icon name="chevron-left" size={24} color="#000" />
+        </TouchableOpacity>
+        <View style={styles.icons}>
+          <TouchableOpacity style={styles.icons}>
+            <Icon name="plus" size={24} color="#000" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.icons}>
+            <Icon name="share" size={24} color="#000" />
+          </TouchableOpacity>
+        </View>
+      </View>
+
       <View>
+        <View style={styles.categoriesBox}>
+          {product?.categories.map(category => (
+            <View style={styles.categoryBox}>
+              <Text style={styles.categoryText}>{category}</Text>
+            </View>
+          ))}
+        </View>
         <Image
           style={styles.productImage}
           source={{
-            uri: 'https://raw.githubusercontent.com/Josimar16/teste-fullstack-mobile/main/images/1%20–%201.png',
+            uri: product?.url_image,
           }}
         />
       </View>
 
       <View style={styles.centralize}>
-        <Text style={styles.productName}>Jordan 5 Nike Style Muito Crazy</Text>
+        <Text style={styles.productName}>{product?.name}</Text>
       </View>
 
       <View style={styles.centralize}>
-        <Text style={styles.productAmount}>R$ 1150,00</Text>
+        <Text style={styles.productAmount}>R$ {product?.amount}</Text>
       </View>
 
       <View>
         <View style={styles.sizesBox}>
-          <Text style={styles.productSize}>35</Text>
-          <Text style={styles.mainProductSize}>36</Text>
-          <Text style={styles.productSize}>37</Text>
+          <Text style={styles.productSize}>
+            {product?.size ? product.size - 1 : 0}
+          </Text>
+          <Text style={styles.mainProductSize}>{product?.size}</Text>
+          <Text style={styles.productSize}>
+            {product?.size ? +product.size + 1 : 0}
+          </Text>
         </View>
         <View style={styles.centralize}>
-          <Text>Tamanho 36</Text>
+          <Text>Tamanho {product?.size}</Text>
         </View>
       </View>
 
@@ -49,8 +97,10 @@ export const ProductDetailsPage = ({ route }: Props) => {
             <Text />
           </View>
           <View>
-            <Text style={styles.titleAmountSize}>R$ 1150,00</Text>
-            <Text style={styles.subtitleAmountSize}>Tamanho 12</Text>
+            <Text style={styles.titleAmountSize}>R$ {product?.amount}</Text>
+            <Text style={styles.subtitleAmountSize}>
+              Tamanho {product?.size}
+            </Text>
           </View>
         </TouchableOpacity>
       </View>
--- mobile/src/screens/productDetail/styles.tsx
+++ mobile/src/screens/productDetail/styles.tsx
@@ -3,10 +3,26 @@ import { Dimensions, StyleSheet } from 'react-native';
 const { height, width } = Dimensions.get('window');
 
 export const styles = StyleSheet.create({
+  container: { justifyContent: 'space-around', flex: 1 },
+  iconsBox: { flexDirection: 'row', justifyContent: 'space-between' },
   centralize: {
     justifyContent: 'center',
     alignItems: 'center',
   },
+  icons: {
+    marginHorizontal: 10,
+    flexDirection: 'row',
+  },
+  categoriesBox: { flexDirection: 'row' },
+  categoryBox: {
+    margin: 5,
+    borderColor: '#AAA',
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 5,
+    height: 25,
+  },
+  categoryText: { fontSize: 12 },
   productImage: {
     width,
     height: height / 2 - 80,
@@ -14,13 +30,13 @@ export const styles = StyleSheet.create({
   productName: {
     color: '#000',
     fontWeight: '700',
-    fontSize: 24,
+    fontSize: 36,
   },
   productAmount: {
     marginTop: 10,
     color: '#000',
     fontWeight: '700',
-    fontSize: 36,
+    fontSize: 32,
   },
   productSize: {
     margin: 20,
