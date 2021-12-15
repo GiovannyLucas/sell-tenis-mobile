38b38593d5e22f9db1ba0d045c0d7229993ac157
GiovannyLucas <giovannylucax@gmail.com>
2021-12-15 00:26:27 -0300

    feat: :sparkles: added route to get a product by id and inserted in the product's module

--- backend/src/modules/products/Products.module.ts
+++ backend/src/modules/products/Products.module.ts
@@ -3,13 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './infra/http/controllers/Products.controller';
 import { Product } from './infra/typeorm/entities/Product';
 import { ProductsRepository } from './infra/typeorm/repositories/ProductsRepository';
-import { FindProductsUseCase } from './useCases/findProductsUseCase/FindProducts.useCase';
+import { FindProductsUseCase } from './useCases/findProducts/FindProducts.useCase';
+import { ShowProductUseCase } from './useCases/showProduct/ShowProduct.useCase';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductsController],
   providers: [
     FindProductsUseCase,
+    ShowProductUseCase,
     {
       provide: 'PRODUCTS_REPOSITORY',
       inject: [ProductsRepository],
--- backend/src/modules/products/infra/http/controllers/Products.controller.ts
+++ backend/src/modules/products/infra/http/controllers/Products.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ShowProductUseCase } from 'src/modules/products/useCases/showProduct/ShowProduct.useCase';
 import { FilterProductsDTO } from '../../../dtos/FilterProductsDTO';
-import { FindProductsUseCase } from '../../../useCases/findProductsUseCase/FindProducts.useCase';
+import { FindProductsUseCase } from '../../../useCases/findProducts/FindProducts.useCase';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private findProductsUseCase: FindProductsUseCase) {}
+  constructor(
+    private findProductsUseCase: FindProductsUseCase,
+    private showProductUseCase: ShowProductUseCase,
+  ) {}
 
   @Get()
   async find(@Query() filters: FilterProductsDTO) {
     const products = await this.findProductsUseCase.execute(filters);
 
     return products;
+  }
+
+  @Get(':id')
+  async show(@Param('id') id: string) {
+    const product = await this.showProductUseCase.execute(id);
+
+    return product;
   }
 }
