15b25151b6dd1fea6623d1613fb52823cb2164da
GiovannyLucas <giovannylucax@gmail.com>
2021-12-15 09:36:05 -0300

    feat: :sparkles: installed initial packages and base structure improved

---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {ProductsPage} from '../screens/products';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppRoutes() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: '#3E4D9D'},
+        contentStyle: {backgroundColor: '#FFF'},
+        headerTintColor: 'white',
+      }}>
+      <Stack.Screen
+        name="Home"
+        component={ProductsPage}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+}
---
+++ mobile/src/screens/products/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+export const ProductsPage = () => {
+  return (
+    <View>
+      <Text>TESTANDO</Text>
+      <Icon name="arrow-right" color="#3E4D9D" size={18} />
+    </View>
+  );
+};
